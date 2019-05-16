f097e7bd650c43842b53442c93d0bf5035c8728f
Otto G <ottodevs@gmail.com>
2019-05-16 17:55:57 +0200

    Initial script

--- app/src/script.js
+++ app/src/script.js
@@ -1,37 +1,100 @@
 import '@babel/polyfill'
-import { of } from 'rxjs'
-import AragonApi from '@aragon/api'
+import Aragon from '@aragon/client'
+import 'rxjs/add/observable/of'
+import { first, pluck } from 'rxjs/operators'
 
-const INITIALIZATION_TRIGGER = Symbol('INITIALIZATION_TRIGGER')
+const app = new Aragon()
+let appState
+app.events().subscribe(handleEvents)
 
-const api = new AragonApi()
+app.state().subscribe(state => {
+  appState = state || { entries: [] }
+})
 
-api.store(
-  async (state, event) => {
-    let newState
+/***********************
+ *                     *
+ *   Event Handlers    *
+ *                     *
+ ***********************/
 
-    switch (event.event) {
-      case INITIALIZATION_TRIGGER:
-        newState = { count: await getValue() }
-        break
-      case 'Increment':
-        newState = { count: await getValue() }
-        break
-      case 'Decrement':
-        newState = { count: await getValue() }
-        break
-      default:
-        newState = state
+async function handleEvents({ event, returnValues }) {
+  let nextState
+  /*
+    IMPORTANT: I'm refreshing all widgets here on every event (EVEN ON STARTUP EMPTY ONES), this is really underperformant. 
+    Should be improved adding a return value with priority index to the smart contract events so we can update just the widget affected. 
+  */
+  switch (event) {
+    case 'WidgetAdded':
+      nextState = await refreshAllWidgets(appState)
+      break
+    case 'WidgetRemoved':
+      nextState = await refreshAllWidgets(appState)
+      break
+    case 'WidgetUpdated':
+      nextState = await refreshAllWidgets(appState)
+      break
+    case 'WidgetsReordered':
+      nextState = await refreshAllWidgets(appState)
+      break
+    default:
+      nextState = await refreshAllWidgets(appState)
+      console.log('[Widgets script] unknown event', event, returnValues)
+  }
+
+  // purify the resulting state to handle duplication edge cases
+  const filteredState = { entries: filterEntries(nextState.entries) }
+  app.cache('state', filteredState)
+}
+
+const refreshAllWidgets = async ({ entries = [] }) => {
+  return new Promise(async resolve => {
+    // Clear all entries
+    entries = []
+    var widgetNumber = await app
+      .call('getWidgetsLength')
+      .pipe(first())
+      .toPromise()
+    for (var i = 0; i <= widgetNumber; i++) {
+      try {
+        var widgetData = await loadWidgetData(i)
+        entries.push(widgetData) // add to the state object received as param
+      } catch (err) {
+        console.log(err)
+      }
     }
+    const state = { entries } // return the entries array
+    console.log(entries)
+    resolve(state)
+  })
+}
 
-    return newState
-  },
-  [
-    // Always initialize the store with our own home-made event
-    of({ event: INITIALIZATION_TRIGGER }),
-  ]
-)
+/***********************
+ *                     *
+ *       Helpers       *
+ *                     *
+ ***********************/
 
-async function getValue() {
-  return parseInt(await api.call('value').toPromise(), 10)
+const loadWidgetData = async priority => {
+  return new Promise((resolve, reject) => {
+    app.call('getWidget', priority).subscribe(
+      widget => {
+        if (!widget) {
+          reject()
+        } else {
+          resolve({
+            addr: widget[0],
+            disabled: widget[1],
+          })
+        }
+      },
+      err => {
+        reject(err)
+      }
+    )
+  })
+}
+
+const filterEntries = entries => {
+  const filteredEntries = entries.filter(entry => entry && !!entry.addr)
+  return filteredEntries
 }
