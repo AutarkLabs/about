6942c0e254d46f00483beba6a6c1e4fa2b34dbda
Otto G <ottodevs@gmail.com>
2019-05-20 14:49:25 +0200

    Contract basic gas optimizations

--- contracts/HomePage.sol
+++ contracts/HomePage.sol
@@ -21,11 +21,20 @@ contract HomePage is AragonApp {
     Widget[] public widgets;
 
     /// ACL
+    /* Hardcoded constants to save gas
     bytes32 constant public ADD_ROLE = keccak256("ADD_ROLE");
     bytes32 constant public REMOVE_ROLE = keccak256("REMOVE_ROLE");
     bytes32 constant public REORDER_ROLE = keccak256("REORDER_ROLE");
     bytes32 constant public UPDATE_ROLE = keccak256("UPDATE_ROLE");
+    */
+    bytes32 constant public ADD_ROLE = 0x9fdb66370b2703c58b55fbb88662023f986df503f838f6ca75ff9f9bdabd694a;
+    bytes32 constant public REMOVE_ROLE = 0x0a55cd9c498e2688378e5cd183217e75c0fa1cdbaa909387f565177cd47670f5;
+    bytes32 constant public REORDER_ROLE = 0x0210352125167815ae2d54bb8e405f542b6cd4763cd039d14f046edad97dc03d;
+    bytes32 constant public UPDATE_ROLE = 0x3ee192ac25847473237ced4bba57f848e1fd794930ff85b42823290580967205;
 
+    /**
+     * @notice Custom constructor to initialize aragon app
+     */
     function initialize() external onlyInit {
         initialized();
     }
@@ -45,7 +54,7 @@ contract HomePage is AragonApp {
      * @param _addr IPFS hash of the widget's data
      */
     function updateWidget(uint _priority, string _addr) external auth(UPDATE_ROLE) {
-        Widget storage widget = widgets[_priority];
+        Widget memory widget = widgets[_priority];
         widget.addr = _addr;
         emit WidgetUpdated();
     }
@@ -55,7 +64,7 @@ contract HomePage is AragonApp {
      * @param _priority Index of the widget
      */
     function removeWidget(uint _priority) external auth(REMOVE_ROLE) {
-        Widget storage widget = widgets[_priority];
+        Widget memory widget = widgets[_priority];
         widget.addr = "";
         widget.deleted = true;
         emit WidgetRemoved();
@@ -77,7 +86,7 @@ contract HomePage is AragonApp {
      * @notice Get the Length of the widgets array
      * @return  length of widgets array
      */
-    function getWidgetsLength() external view returns(uint) {
+    function getWCount() external view returns(uint) {
         return widgets.length;
     }
 
@@ -86,9 +95,8 @@ contract HomePage is AragonApp {
      * @param  _priority index of the widget
      */
     function getWidget(uint _priority) external view returns(string addr, bool deleted) {
-        Widget storage widget = widgets[_priority];
+        Widget memory widget = widgets[_priority];
         addr = widget.addr;
         deleted = widget.deleted;
     }
-
 }
