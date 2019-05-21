15042ac9f0823ea92a75f3c83ac7cf2c957ff3ba
mat <mat@localhost.localdomain>
2019-05-21 23:58:02 +0200

    Add EditorToolbar with dummy icons

--- app/src/components/PanelContent.js
+++ app/src/components/PanelContent.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Button, DropDown, Field, SidePanelSeparator, TabBar } from '@aragon/ui'
-
+import Octicon, {Bold, Italic, TextSize, Quote, Code, Link, ListUnordered} from '@githubprimer/octicons-react'
 import Editor from './Editor'
 import Preview from './Preview'
+import {
+  IconAdd,
+} from '@aragon/ui'
 
 const initialState = {
   screenIndex: 0,
+  codemirrorInstance:null,
   unsavedText: `
   # Title
 
@@ -57,6 +61,14 @@ class PanelContent extends React.Component {
     this.setState({ unsavedText })
   }
 
+  onCodeMirrorInit = codemirrorInstance => {
+    this.setState({ codemirrorInstance })
+  }
+
+  setSelectionBold = () => {
+    this.state.codemirrorInstance.doc.replaceSelection("**"+this.state.codemirrorInstance.doc.getSelection()+"**");
+  }
+
   render() {
     const { screenIndex, unsavedText } = this.state
     // const { opened, tokens, onWithdraw, onDeposit, proxyAddress } = this.props
@@ -68,6 +80,7 @@ class PanelContent extends React.Component {
           flex: '1',
         }}
       >
+
         <Field label="Type">
           <DropDown
             items={[
@@ -79,7 +92,7 @@ class PanelContent extends React.Component {
             onChange={() => {}}
           />
         </Field>
-        <SidePanelSeparator style={{ margin: '15px -30px' }} />
+        <SidePanelSeparator style={{ margin: '15px -30px 5px -30px' }} />
         <TabBarWrapper>
           <div
             style={{
@@ -94,7 +107,17 @@ class PanelContent extends React.Component {
               selected={screenIndex}
               onChange={this.handleChange}
             />
-            Edit ToolBar
+            <EditToolBar>
+              <EditToolBarButton onClick={this.setSelectionBold} compact><Octicon icon={TextSize}/></EditToolBarButton>
+              <EditToolBarButton onClick={this.setSelectionBold} compact><Octicon icon={Bold}/></EditToolBarButton>
+              <EditToolBarButton onClick={this.setSelectionBold} compact><Octicon icon={Italic}/></EditToolBarButton>
+              <EditToolBarSeparator />
+              <EditToolBarButton onClick={this.setSelectionBold} compact><Octicon icon={Quote}/></EditToolBarButton>
+              <EditToolBarButton onClick={this.setSelectionBold} compact><Octicon icon={Code}/></EditToolBarButton>
+              <EditToolBarButton onClick={this.setSelectionBold} compact><Octicon icon={Link}/></EditToolBarButton>
+              <EditToolBarButton onClick={this.setSelectionBold} compact><Octicon icon={ListUnordered}/></EditToolBarButton>
+              <EditToolBarSeparator />
+            </EditToolBar>
           </div>
         </TabBarWrapper>
         <SidePanelSeparator
@@ -104,7 +127,7 @@ class PanelContent extends React.Component {
         />
 
         <div style={{ flex: '1' }}>
-         {screenIndex === 0 && <Editor value={unsavedText} onChange={this.handleEditorChange} />} 
+          {screenIndex === 0 && <Editor value={unsavedText} onChange={this.handleEditorChange} onCodeMirrorInit={this.onCodeMirrorInit}/>} 
           {screenIndex === 1 && <Preview value={unsavedText} />}
         </div>
         <SidePanelSeparator style={{ margin: '15px -30px' }} />
@@ -118,6 +141,23 @@ class PanelContent extends React.Component {
 
 const TabBarWrapper = styled.div`
   margin: 0 -30px 30px;
+`
+
+const EditToolBar = styled.div`
+  margin-top:-2px;
+`
+
+const EditToolBarButton = styled(Button)`
+  width:22px;
+  height:22px;
+  text-align:center;
+  padding:0;
+  margin:0;
+`
+const EditToolBarSeparator = styled.div`
+  display:inline-block;
+  width:10px;
+  height:1px;
 `
 
 export default PanelContent
