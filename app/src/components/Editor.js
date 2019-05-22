951fb0185ef3964c6ba2b603b61e4be1bf0985cd
Otto G <ottodevs@gmail.com>
2019-05-22 16:56:57 +0200

    Fix background color on preview, fix editor

--- app/src/components/Editor.js
+++ app/src/components/Editor.js
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { Controlled as CodeMirror } from 'react-codemirror2'
 import 'codemirror/lib/codemirror.css'
-import 'codemirror/mode/markdown/markdown'
-// Dark mode css
-//import 'codemirror/theme/material.css'
+import 'codemirror/mode/gfm/gfm'
+import 'codemirror/theme/elegant.css'
+// ambiance ambiance-mobile elegant idea neat neo ttcn xq-light 3024-day
 
 // import dompurify from 'dompurify';
 
@@ -14,7 +14,8 @@ const Editor = ({ content, instance, onCodeMirrorInit, onChange }) => {
   }, [instance])
 
   return (
-    <div style={{
+    <div
+      style={{
         width: '100%',
         height: '100%',
         bottom: '0',
@@ -25,8 +26,8 @@ const Editor = ({ content, instance, onCodeMirrorInit, onChange }) => {
       <CodeMirror
         value={content}
         options={{
-          mode: 'markdown',
-          theme: 'material',
+          mode: 'gfm',
+          theme: 'elegant',
           lineWrapping: true,
         }}
         editorDidMount={editor => {
