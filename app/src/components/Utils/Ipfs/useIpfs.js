12b1d5b2f4270a658e8e47f1e4a9cd9e30439e76
Otto G <ottodevs@gmail.com>
2019-05-29 16:03:45 +0200

    Fix imports and wrong propTypes

--- app/src/components/Utils/Ipfs/useIpfs.js
+++ app/src/components/Utils/Ipfs/useIpfs.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react'
-//import axios from 'axios'
 import ipfsClient from 'ipfs-http-client'
-
 
 const useIpfs = ipfsOptions => {
   const [ipfsInstance, setIpfsInstance] = useState(null)
