da0a60881f23be63d38af059f02db20da54699b8
mateo <mateotomasgomez@gmail.com>
2019-05-29 15:33:58 +0200

    Add ipfs hooks

---
+++ app/src/components/Utils/Ipfs/ipfsAdd.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import useIpfs from './useIpfs'
+import ipfsConfig from '../../../../ipfs'
+
+const bufferFile = content => Buffer.from(JSON.stringify(content))
+
+const ipfsAdd = () => {
+  const [ipfsClient] = useIpfs(ipfsConfig)
+  const [data, setData] = useState(null)
+  const [ipfsAddr, setIpfsAddr] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  const fetchData = async () => {
+    setIsError(false)
+    setIsLoading(true)
+    setIpfsAddr(null)
+    const file = bufferFile(data)
+    try {
+      const result = await ipfsClient.add(file)
+      setIpfsAddr(result[0].hash)
+    } catch (err) {
+      console.error('Error pinning file to IPFS', err)
+      setIsError(true)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    if (data !== null) {
+      fetchData()
+    }
+  }, [data])
+
+  return [{ ipfsAddr, isLoading, isError }, setData]
+}
+export default ipfsAdd
