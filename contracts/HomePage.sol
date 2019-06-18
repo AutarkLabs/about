f182e162ef3d623e7f77f92bf46c9081b4382c22
Mateo Tomas <mateotomasgomez@gmail.com>
2019-06-18 18:29:05 +0200

    Add new contract
    New contract handles entries with mapping instead of a simple array.
    The other way will cause a problem when users add sidebar widget before main widget.

--- contracts/HomePage.sol
+++ contracts/HomePage.sol
@@ -18,7 +18,7 @@ contract HomePage is AragonApp {
     }
 
     /// State
-    Widget[] public widgets;
+    mapping(bytes32 => Widget) internal widgets;
 
     /// ACL
     /* Hardcoded constants to save gas
@@ -40,61 +40,20 @@ contract HomePage is AragonApp {
     }
 
     /**
-     * @notice Add a widget to the registry https://ipfs.infura.io/ipfs/`_addr`
-     * @param _addr IPFS hash of the widget's data
-     */
-    function addWidget(string _addr) external auth(ADD_ROLE) {
-        widgets.push(Widget(_addr, false));
-        emit WidgetAdded();
-    }
-
-    /**
      * @notice Update widget `_priority` to https://ipfs.infura.io/ipfs/`_addr`
      * @param _priority Index of the widget
      * @param _addr IPFS hash of the widget's data
      */
-    function updateWidget(uint _priority, string _addr) external auth(UPDATE_ROLE) {
-        Widget memory widget = widgets[_priority];
-        widget.addr = _addr;
+    function updateWidget(bytes32 _priority, string _addr) external auth(UPDATE_ROLE) {
+        widgets[_priority] = Widget(_addr, false);
         emit WidgetUpdated();
-    }
-
-    /**
-     * @notice Remove a widget from the registry
-     * @param _priority Index of the widget
-     */
-    function removeWidget(uint _priority) external auth(REMOVE_ROLE) {
-        Widget memory widget = widgets[_priority];
-        widget.addr = "";
-        widget.deleted = true;
-        emit WidgetRemoved();
-    }
-
-    /**
-     * @notice Swap Two widgets
-     * @param _priority1 index of the first widget to be swapped
-     * @param _priority2 index of the second widget to be swapped
-     */
-    function reorderWidgets(uint _priority1, uint _priority2) external auth(REORDER_ROLE) {
-        Widget memory temp = widgets[_priority1];
-        widgets[_priority1] = widgets[_priority2];
-        widgets[_priority2] = temp;
-        emit WidgetsReordered();
-    }
-
-    /**
-     * @notice Get the Length of the widgets array
-     * @return  length of widgets array
-     */
-    function getWCount() external view returns(uint) {
-        return widgets.length;
     }
 
     /**
      * @notice Get a widget's information
      * @param  _priority index of the widget
      */
-    function getWidget(uint _priority) external view returns(string addr, bool deleted) {
+    function getWidget(bytes32 _priority) external view returns(string addr, bool deleted) {
         Widget memory widget = widgets[_priority];
         addr = widget.addr;
         deleted = widget.deleted;
