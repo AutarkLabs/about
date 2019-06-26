42408a8de9927984970d3df42aa2a9efae9e1fb4
Mateo Tomas <mateotomasgomez@gmail.com>
2019-06-26 18:18:23 +0200

    Small style fix on loading overlay

--- app/src/components/panel/PanelContent.js
+++ app/src/components/panel/PanelContent.js
@@ -280,7 +280,7 @@ const LoadingOverlay = styled.div`
   position: absolute;
   top: 0;
   bottom: 0;
-  left: 0;
+  left: -15px;
   right: 0;
   background: rgba(255, 255, 255, 0.8);
   z-index: 10;
