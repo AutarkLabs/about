b671976ea6e52b056e14f42c064ee4d55956907d
Mateo Tomas <mateotomasgomez@gmail.com>
2019-06-27 11:00:33 +0200

    Fix padding of other widget types #25

--- app/src/styles.js
+++ app/src/styles.js
@@ -3,18 +3,7 @@ import styled from 'styled-components'
 const SideBarScrollbarContainer = styled.div`
   height: 100%;
   position: relative;
-
-  .react-codemirror2 {
-    height: 100%;
-    display: block;
-    position: relative;
-    cursor: text;
-  }
-  .CodeMirror {
-    padding: 15px 30px;
-    min-height: 100%;
-    overflow: hidden;
-  }
+  padding: 15px 30px;
 `
 
 const MarkdownContainer = styled.div`
