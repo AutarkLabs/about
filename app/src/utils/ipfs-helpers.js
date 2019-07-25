f8228fb5bd164233e11b1b9802f6392b483ef5d8
Otto G <ottodevs@gmail.com>
2019-07-25 07:07:25 +0200

    Refactor ipfs-helpers

--- app/src/utils/ipfs-helpers.js
+++ app/src/utils/ipfs-helpers.js
@@ -1,23 +1,20 @@
 import axios from 'axios'
-import dompurify from 'dompurify'
 import ipfsClient from 'ipfs-http-client'
 
-const ipfsConfig = {
+let autarkIpfs = ipfsClient({
   host: 'ipfs.autark.xyz',
-  path: '/api/v0',
   port: '5001',
   protocol: 'https',
-}
-
-const ipfsInstance = ipfsClient(ipfsConfig)
+})
+const ipfsEndpoint = 'https://ipfs.autark.xyz:5001/api/v0'
 
 const bufferFile = content =>
-  ipfsInstance.types.Buffer.from(dompurify.sanitize(content))
+  autarkIpfs.types.Buffer.from(JSON.stringify(content))
 
 export const ipfsAdd = async content => {
   const file = bufferFile(content)
   try {
-    const result = await ipfsInstance.add(file)
+    const result = await autarkIpfs.add(file)
     return result[0].hash
   } catch (err) {
     console.error('Error pinning file to IPFS', err)
@@ -25,8 +22,7 @@ export const ipfsAdd = async content => {
 }
 
 export const ipfsGet = async hash => {
-  const { host, port, protocol, path } = ipfsConfig
-  const endpoint = `${protocol}://${host}:${port}${path}/cat?arg=${hash}`
+  const endpoint = `${ipfsEndpoint}/cat?arg=${hash}`
   try {
     const { data } = await axios.get(endpoint)
     return data
